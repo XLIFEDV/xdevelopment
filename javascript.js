dbf95e668a34cfe445f294109e8b3a0e292fc7df
XDEVELOPMENT <contact@xdevelopment.dev>
2026-01-13 21:38:50 +0300

    update

--- javascript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        $('body').addClass('mobile');
-    } else {
-        $('body').addClass('computer');
-    };
-});
-document.getElementById("y").textContent = new Date().getFullYear();
